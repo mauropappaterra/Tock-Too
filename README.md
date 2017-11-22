7e4bc7ba7a6a53b364dd08e9a9142970649ffd9b
yogurt1989 <mauropappaterra@gmail.com>
2017-11-22 18:01:26 +0100

    Updated error handling, imported sys module

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Tock-Too
 A (very) short program to test the TOCTTOU problem.
-TOCTTOU stands for Time of check to time of use.
-More info: https://en.wikipedia.org/wiki/Time_of_check_to_time_of_use
+TOCTTOU stands for Time of check to time of use, for more info: https://en.wikipedia.org/wiki/Time_of_check_to_time_of_use
--- TOCTTOU.py
+++ TOCTTOU.py
@@ -1,14 +1,14 @@
 # Tock Too 
 # TOCTTOU.py
 # Created by Mauro J. Pappaterra on 20 of November 2017.
+import sys
 
 path = "file.txt"
 mode = "a" # append mode
 
 try:
     with open (path, mode) as file:
-
-        print("\nFile open for writing. You can start writing the document below.\nWrite <done> in a new line to finish editing\n")
+        print("\nFile open for writing. You can start writing the document below.\nWrite <done> in a new line to finish editing!\n")
         text = ""
         line = ""
 
@@ -18,13 +18,10 @@ try:
 
         file.write(text[:-7])
         print("\nAll changes saved into " + path)
+#except:
+#    print ("\nError could not write into file " + path)
+except IOError as e:
+    print ("I/O error({0}): {1}".format(e.errno, e.strerror))
 except:
-    print ("\nError could not write into file " + path)
-
-
-
-
-
-
-
-
+    print ("Unexpected error:", sys.exc_info()[0])
+    raise
