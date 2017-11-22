78ac5ca2eaf5ad88013b5067e35daa75d2cf8f75
yogurt1989 <mauropappaterra@gmail.com>
2017-11-22 17:28:17 +0100

    First Push, create file and write

--- TOCTTOU.py
+++ TOCTTOU.py
@@ -1,3 +1,26 @@
 # Tock Too 
 # TOCTTOU.py
-# Created by Mauro J. Pappaterra on 20 of 11 2017.
+# Created by Mauro J. Pappaterra on 20 of November 2017.
+
+from pathlib import Path # import Path module from library
+
+path = "file.txt"
+mode = "w" # write mode
+
+with open (path, mode) as file:
+
+    print("File open for writing. You can start writing the document below. Write <done> to finish editing")
+    text = ""
+    line = ""
+
+    while (line != "<done>"):
+        line = input()
+        text += line + "\n"
+
+    file.write(text[:-8])
+
+
+
+
+
+
