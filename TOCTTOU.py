af79c7bf96c7558428cec8b7fc02084e10e71987
yogurt1989 <mauropappaterra@gmail.com>
2017-11-22 17:46:35 +0100

    Added error handling, deleted unecessary imports

--- TOCTTOU.py
+++ TOCTTOU.py
@@ -2,24 +2,26 @@
 # TOCTTOU.py
 # Created by Mauro J. Pappaterra on 20 of November 2017.
 
-from pathlib import Path # import Path module from library
-
 path = "file.txt"
-mode = "a" # write mode
+mode = "a" # append mode
 
-with open (path, mode) as file:
+try:
+    with open (path, mode) as file:
 
-    print("\nFile open for writing. You can start writing the document below.\nWrite <done> in a new line to finish editing\n")
-    text = ""
-    line = ""
+        print("\nFile open for writing. You can start writing the document below.\nWrite <done> in a new line to finish editing\n")
+        text = ""
+        line = ""
 
-    while (line != "<done>"):
-        line = input()
-        text += line + "\n"
+        while (line != "<done>"):
+            line = input()
+            text += line + "\n"
 
-    file.write(text[:-7])
+        file.write(text[:-7])
+        print("\nAll changes saved into " + path)
+except:
+    print ("\nError could not write into file " + path)
 
-print ("\nAll changes saved into " + path)
+
 
 
 
